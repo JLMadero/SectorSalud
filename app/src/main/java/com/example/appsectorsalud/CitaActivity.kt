6e20777a9f8719032a9d9d299404a7a8703127e0
Diego <haloalcantar@gmail.com>
2025-04-28 21:38:59 -0700

    button navigator

--- app/src/main/java/com/example/appsectorsalud/CitaActivity.kt
+++ app/src/main/java/com/example/appsectorsalud/CitaActivity.kt
@@ -1,20 +1,35 @@
 package com.example.appsectorsalud
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.appsectorsalud.databinding.ActivityCitaBinding
 
 class CitaActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCitaBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cita)
 
-        findViewById<Button>(R.id.btnSolicitarCita).setOnClickListener {
-            Toast.makeText(this, "Cita solicitada con el Dr. Pérez", Toast.LENGTH_LONG).show()
-        }
+        binding = ActivityCitaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_cita)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
