736c76c58bc73becfb7cf7a68b3aec5f9400d35b
JLMadero <madero26jose08@gmail.com>
2025-04-28 22:39:13 -0700

    back del cliente

---
+++ Backend/src/main/java/com/mycompany/backend/Expediente.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.backend;
+
+/**
+ *
+ * @author jl4ma
+ */
+public class Expediente {
+    
+     private String pacienteId;
+    private String nombre;
+    private String diagnostico;
+    // Agrega los campos que necesites
+
+    // Getters y setters
+    public String getPacienteId() {
+        return pacienteId;
+    }
+
+    public void setPacienteId(String pacienteId) {
+        this.pacienteId = pacienteId;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDiagnostico() {
+        return diagnostico;
+    }
+
+    public void setDiagnostico(String diagnostico) {
+        this.diagnostico = diagnostico;
+    }
+}
